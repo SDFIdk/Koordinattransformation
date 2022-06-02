2ef460e7772e741804466921d73b4d0d69f2e23b
Frank R Thomsen <frtho@sdfe.dk>
2022-06-02 14:50:07 +0200

    new config setup yay

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
+require('dotenv').config({ path: `../.env.${ process?.env?.NODE_ENV || '' }` })
 module.exports = defineConfig({
   transpileDependencies: true
 })

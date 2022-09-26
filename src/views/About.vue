774d13bcb6ec879abedc77aca9eb25282763e5d8
David Vinje <davin@sdfe.dk>
2022-09-26 14:19:29 +0200

    style the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,27 +2,16 @@
   <div class="container">
     <h1>Koordinattransformation</h1>
     <h3>Udviklet af <a href="https://sdfi.dk/" target="_blank">SDFI</a>.</h3>
+    <h4>Muliggør transformation mellem forskellige aktuelle og historisk anvendte koordinatsystemer.</h4>
     <h4>
-      Muliggør transformation mellem forskellige aktuelle og historisk anvendte koordinatsystemer.
-      <br>
-      <br>
-      Vælg et input- og outputkoordinatsystem. Vælg derefter et sæt inputkoordinater for at fortage transformationen.
-      <br>
-      <br>
-      Inputkoordinater vælges ved enten at indtaste dem selv, trykke på kortet, eller indtaste en addresse i søgefeltet.
-      <br>
-      <br>
-      Bruger det valgte koordinatsystem enheden decimalgrader (DD), kan koordinaterne formateres mellem grader, decimalminutter (min.) og grader, minutter, sekunder (min. sek.).
-      <!-- <br>
-      <br> -->
-      Ellers vil enheden stå som meter [m].
-      <br>
-      <br>
-      Når de ønskede outputkoordinater er fundet, kan du trykke på 'Kopiér'-knappen og få koordinaterne kopieret til udklipsholderen.
-      <br>
-      <br>
-      <a href="https://dataforsyningen.dk/help">Kontakt support for hjælp.</a>
+      1. Vælg et input- og outputkoordinatsystem. Vælg derefter et sæt inputkoordinater for at fortage transformationen.
+      <div><h4>Inputkoordinater vælges ved enten at indtaste dem selv, trykke på kortet, eller indtaste en addresse i søgefeltet.</h4></div>
     </h4>
+    <h4>
+      2. Bruger det valgte koordinatsystem enheden decimalgrader (DD), kan koordinaterne formateres mellem grader, decimalminutter (min.) og grader, minutter, sekunder (min. sek.).
+      <div><h4>Ellers vil enheden stå som meter [m].</h4></div>
+    </h4>
+    <h4>3. Når de ønskede outputkoordinater er fundet, kan du trykke på 'Kopiér'-knappen og få koordinaterne kopieret til udklipsholderen.</h4>
     <h6>Powered by <a href="https://docs.dataforsyningen.dk/#webproj" target="_blank">Webproj</a></h6>
   </div>
 </template>
@@ -34,6 +23,9 @@ export default {
 </script>
 
 <style scoped>
+ol > li {
+    margin-left: 10px;
+}
 .container {
   margin-left: 5rem;
 }

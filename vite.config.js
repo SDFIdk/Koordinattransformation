73cb2e9f6977a241ecd5e0a4ad83a090acc03562
PelleSwelle <pedoe@sdfe.dk>
2023-01-27 10:12:20 +0100

    added vitest for testing

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 const path = require('path')
 
 export default defineConfig({
+  test: {
+
+  },
   plugins: [vue()],
   resolve: {
     alias: {

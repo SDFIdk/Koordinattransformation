d2f2431855b1167a0c90e64ae26373f04499ca12
Peter Dønvang <B177974@PROD.SITAD.DK>
2023-06-21 12:01:12 +0200

    correct loading animation, proper icon implementation

--- src/components/coordinatetransformation/OutputCard.vue
+++ src/components/coordinatetransformation/OutputCard.vue
@@ -11,7 +11,7 @@
         </section>
         <div class="transformed-coordinates" :class="{ hasTransformed: hasTransformed}">
             <div v-if="isLoading">
-                <Loader size="1.5" :isLoading=isLoading />
+                <Loader :isLoading=isLoading />
             </div>
             <div v-else>
                 <div class="output-coordinates">{{ output1 }}</div>
@@ -47,17 +47,7 @@
                 </div>
             <button class="copy-btn" @click="copyCoordinates" :class="{hasTransformed: hasTransformed && !isLoading}">
                 Kopiér
-                <span v-show="!hasTransformed || isLoading">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M7.30769 15H6.53846C6.13044 15 5.73912 14.8379 5.4506 14.5494C5.16209 14.2609 5 13.8696 5 13.4615V6.53846C5 6.13044 5.16209 5.73912 5.4506 5.4506C5.73912 5.16209 6.13044 5 6.53846 5H13.4615C13.8696 5 14.2609 5.16209 14.5494 5.4506C14.8379 5.73912 15 6.13044 15 6.53846V7.30769M10.5385 9.00001H17.4615C18.3112 9.00001 19 9.6888 19 10.5385V17.4615C19 18.3112 18.3112 19 17.4615 19H10.5385C9.68879 19 9 18.3112 9 17.4615V10.5385C9 9.6888 9.68879 9.00001 10.5385 9.00001Z" stroke="hsla(186,25%,64%,0.6)" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                </span>
-
-                <span v-show="hasTransformed && !isLoading">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M7.30769 15H6.53846C6.13044 15 5.73912 14.8379 5.4506 14.5494C5.16209 14.2609 5 13.8696 5 13.4615V6.53846C5 6.13044 5.16209 5.73912 5.4506 5.4506C5.73912 5.16209 6.13044 5 6.53846 5H13.4615C13.8696 5 14.2609 5.16209 14.5494 5.4506C14.8379 5.73912 15 6.13044 15 6.53846V7.30769M10.5385 9.00001H17.4615C18.3112 9.00001 19 9.6888 19 10.5385V17.4615C19 18.3112 18.3112 19 17.4615 19H10.5385C9.68879 19 9 18.3112 9 17.4615V10.5385C9 9.6888 9.68879 9.00001 10.5385 9.00001Z" stroke="hsl(171,70%,55%)" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                </span>
+                <CopyIcon/>
             </button>
         </article>
     </section>
@@ -71,10 +61,11 @@ import { inject, onMounted, ref, watch } from 'vue'
 import { useStore } from 'vuex'
 import { useRoute } from 'vue-router'
 import Formatter from './Formatting'
-import Loader from '@/components/shared/Loader.vue'
 import InfoIcon from '../shared/icons/InfoIcon.vue'
+import CopyIcon from '../shared/icons/CopyIcon.vue'
 const store = useStore()
 const outputEPSG = ref('')
+import Loader from '../shared/Loader.vue'
 
 const format = ref('')
 const outputSelected = ref(false)
--- src/components/shared/icons/CopyIcon.vue
+++ src/components/shared/icons/CopyIcon.vue
@@ -1,34 +1,7 @@
 <template>
-  <svg
-    viewBox="0 0 24 24"
-  >
-    <path
-      :stroke="color"
-      :stroke-width="strokeWidth"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M17.5 9h-7C9.7 9 9 9.7 9 10.5v7c0 .8.7 1.5 1.5 1.5h7c.8 0 1.5-.7 1.5-1.5v-7c0-.8-.7-1.5-1.5-1.5z"
-    />
-    <path
-      :stroke="color"
-      :stroke-width="strokeWidth"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M7.3 15h-.8A1.5 1.5 0 015 13.5v-7A1.5 1.5 0 016.5 5h7A1.5 1.5 0 0115 6.5v.8"
-    />
-  </svg>
+    <svg width="24" height="24" viewBox="0 0 24 24" class="ds-svg-icon" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M7.30769 15H6.53846C6.13044 15 5.73912 14.8379 5.4506 14.5494C5.16209 14.2609 5 13.8696 5 13.4615V6.53846C5 6.13044 5.16209 5.73912 5.4506 5.4506C5.73912 5.16209 6.13044 5 6.53846 5H13.4615C13.8696 5 14.2609 5.16209 14.5494 5.4506C14.8379 5.73912 15 6.13044 15 6.53846V7.30769M10.5385 9.00001H17.4615C18.3112 9.00001 19 9.6888 19 10.5385V17.4615C19 18.3112 18.3112 19 17.4615 19H10.5385C9.68879 19 9 18.3112 9 17.4615V10.5385C9 9.6888 9.68879 9.00001 10.5385 9.00001Z"
+            stroke="hsl(0, 100%, 0%)" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
 </template>
-
-<script>
-export default {
-  name: 'CopyIcon',
-  props: {
-    color: {
-      type: String
-    },
-    strokeWidth: {
-      type: [Number, String]
-    }
-  }
-}
-</script>

e7b29ab0c600a7c0fd515a0263e58df1cc7a1fc5
Andreas Andrä-Fredsted <andan@sdfi.dk>
2024-12-11 16:05:22 +0100

    implemented initial coordinate representation

--- src/components/koortransform/KoorOutputField.vue
+++ src/components/koortransform/KoorOutputField.vue
@@ -1,30 +1,62 @@
 <template>
-    <p>{{ outputCoor }}</p>
-    </template>
-    
-    <script setup>
-    //meter, grader, grader+minutter, grader+minutter+sekunder
-    import { computed, watch } from 'vue'
-    import { useStore } from 'vuex'
-    const store = useStore()
-    
-    const outputCoor = computed(() => store.getters['getCoordinatesTo'])
-    const inputCoor = computed(() => store.getters['getCoordinatesFrom'])
-    
-    const props = defineProps({
-        coordinateFormat: {
-            type: String,
-            default: ''
-        }
-    })
+  <div class="KT-output-container">
+    <svg class="KT-icon">
+      <use href="@/assets/icons/icons.svg#direction-east" />
+    </svg>
+    <span class="KT-coordinate">{{ outputCoor.v1 }}</span>
+    <svg class="KT-icon">
+      <use href="@/assets/icons/icons.svg#direction-north" />
+    </svg>
+    <span class="KT-coordinate">{{ outputCoor.v2 }}</span>
+  </div>
+</template>
 
-    watch(inputCoor, (to, from) => {
-        store.dispatch('setCoordinatesTo')
-    })
-    
-    </script>
-    
-    <style scoped>
-    
-    
-    </style>
+<script setup>
+import { computed, watch, ref } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+// Get the coordinates from the store
+const coorTo = computed(() => store.getters['getCoordinatesTo'])
+const coorFrom = computed(() => store.getters['getCoordinatesFrom'])
+const outputCoor = ref({ v1: null, v2: null, v3: null, v4: null })
+
+const props = defineProps({
+  coordinateFormat: {
+    type: String,
+    default: ''
+  }
+})
+
+// Convert coordinates into a desired representation
+const toRepresentation = (coordinates, repr) => {
+  let res = { v1: null, v2: null, v3: null, v4: null }
+  if (repr === 'direct') {
+    res.v1 = coordinates.v1
+    res.v2 = coordinates.v2
+    res.v3 = coordinates.v3
+    res.v4 = coordinates.v4
+    return res
+  }
+  return { v1: 0.0, v2: 0.0, v3: 0.0, v4: 0.0 }
+}
+
+// Watchers for changes in input/output coordinates
+watch(coorFrom, () => {
+  store.dispatch('setCoordinatesTo')
+})
+
+watch(coorTo, () => {
+  outputCoor.value = toRepresentation(store.getters['getCoordinatesTo'], 'direct')
+})
+</script>
+
+<style scoped>
+.coordinate-container {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+</style>

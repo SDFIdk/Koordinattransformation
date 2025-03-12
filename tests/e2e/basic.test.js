b726dc209a91dd7df9d2cb515120290548f795da
Andreas Andrä-Fredsted <andan@sdfi.dk>
2025-03-12 15:11:20 +0100

    fix: display correct coordinate format for minutes and seconds in output

--- tests/e2e/basic.test.js
+++ tests/e2e/basic.test.js
@@ -1,18 +1,18 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('Page Load', () => {
-  test('Should render Denmark', async ({ page }) => {
+  test('Store Setup Denmark', async ({ page }) => {
     // Wait until the DOM content is loaded, not all resources
     await page.goto('http://localhost:4173/Denmark', { waitUntil: 'domcontentloaded' })
     
     try {
-      // Now wait for your map element, but with a shorter timeout
+      // The #c1 element only loads then the store has been set up succesfully
       await page.waitForSelector('#c1', { timeout: 5000 })
     } catch (error) {
       console.warn('Map did not fully load within 5 seconds. Continuing the test...')
     }
   })
-  test('Should render Greenland', async ({ page }) => {
+  test('Store Setup Greenland', async ({ page }) => {
     // Wait until the DOM content is loaded, not all resources
     //is important to wait for domcontentloaded, as default will wait forever if one call to WMS hangs
     await page.goto('http://localhost:4173/Greenland', { waitUntil: 'domcontentloaded' })
@@ -28,8 +28,26 @@ test.describe('Page Load', () => {
 })
 
 test.describe('Map Tests', () => {
-  //waits for map to be loaded and prints errors if not
-  test('Map Load', async ({page}) => {
+  //test for map Load(inferred from the fact that successfull communication with webmap exists)
+  test('Map Connect Denmark', async({page}) => {
+    await page.goto('http://localhost:4173', { waitUntil: 'domcontentloaded' })
+    const response = await page.waitForResponse( response => 
+      response.url().includes('https://services.datafordeler.dk/DKskaermkort/topo_skaermkort_daempet/1.0.0/wmts') &&
+      response.status() === 200
+    )
+    expect(response.ok()).toBeTruthy()
+  })
+
+  test('Map Connect Greenland', async({page}) => {
+    await page.goto('http://localhost:4173/Greenland', { waitUntil: 'domcontentloaded' })
+    const response = await page.waitForResponse( response => 
+      response.url().includes('https://api.dataforsyningen.dk/wms/gl_aabent_land') &&
+      response.status() === 200
+    )
+    expect(response.ok()).toBeTruthy()
+  })
+  
+  test('Map Click', async ({page}) => {
     let error_msg = []
     page.on('console', msg => {
       if (msg.type() === 'error') {
@@ -59,15 +77,26 @@ test.describe('Map Tests', () => {
 
       const inputSelector = '#c1'
       //allows for multiple attempts
-      await page.waitForFunction(
+      const inputValue= await page.waitForFunction(
         async (selector) => {
-          // Perform the click action
-          // Retrieve the current value of the input field
           const input = document.querySelector(selector)
           return input && input.value !== '0.0000'
         },
         inputSelector
       )
+
+      const outputSelector = '#KT-output'
+
+      const outputValue = await page.waitForFunction(
+        (selector) => {
+          const output = document.querySelector(selector)
+          return output && output.innerHTML.trim() !== ''
+        },
+        outputSelector
+      )
+
+      expect(inputValue.jsonValue()).toBeTruthy()
+      expect(outputValue.jsonValue()).toBeTruthy()
 
     } catch (error) {
       error_msg.forEach(element => {

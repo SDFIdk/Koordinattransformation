55ee3f7547898a06c3fa611c127d044eb3f833a3
Andreas Andrä-Fredsted <andan@sdfi.dk>
2025-03-20 13:28:06 +0100

    chore: updated text on aboutview

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,29 +1,30 @@
 <template>
   <div
     data-theme="light"
-    class="container"
+    class="ds-container about"
   >
     <h1>Koordinattransformation</h1>
+
     <h3>
       Udviklet af <a
         href="https://kds.dk/"
         target="_blank"
       >Klimadatastyrelsen</a>.
     </h3>
-    <h4>Muliggør transformation mellem forskellige aktuelle og historisk anvendte koordinatsystemer.</h4>
-    <ol>
-      <li>
-        Vælg et input- og outputkoordinatsystem. Vælg derefter et sæt inputkoordinater for at foretage transformationen. <br>
-        Inputkoordinater vælges ved enten at indtaste dem selv, trykke på kortet, eller indtaste en addresse i søgefeltet.
-      </li>
-      <li>
-        Bruger det valgte koordinatsystem enheden decimalgrader (DD), kan koordinaterne formateres mellem grader, decimalminutter (min.) og grader, minutter, sekunder (min. sek.).
-        <br> Ellers vil enheden stå som meter [m].
-      </li>
-      <li>
-        Når de ønskede outputkoordinater er fundet, kan du trykke på 'Kopiér'-knappen og få koordinaterne kopieret til udklipsholderen.
-      </li>
-    </ol>
+    <p>
+      Denne webapplikation tilbyder et simpelt interface til transformation af koordinater i forskellige koordinatsystemer. Der kan vælges mellem de mest udbredte koordinatsystemer brugt i Danmark og Grønland.
+      Læs mere om koordinattransformationer på <a href="https://www.klimadatastyrelsen.dk/kortlaegning/geodaesi/koordinattransformation">Klimadatastyrelsens webside</a>
+    </p>
+
+    <h4>Vejledning</h4>
+    <p>
+      Start med at vælge hvilke koordinatsystemer der skal transformeres mellem. Vælg listerne i henholdvis input- og outputsektionerne. Herefter gives en inputkoordinat. 
+      Dette kan gøres enten ved at indtaste i koordinatfeltet, klikke på en lokation på koortet eller ved at indtaste en adresse.
+      <br>
+      Øverst til højre kan skiftes mellem Danmark og Grønland.
+    </p>
+
+
     <h6>
       <router-link
         to="/was"
